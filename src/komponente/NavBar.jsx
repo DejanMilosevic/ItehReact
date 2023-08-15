a8647ce178397bae4304d05e0b3a263e98e05348
Dejan Milosevic <milosevicdejan65@gmail.com>
2023-08-15 19:38:30 +0200

    Napravljena komponenta NavBar

---
+++ src/komponente/NavBar.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const navbarStyle = {
+  backgroundColor: "#f2f2f2",
+  padding: "10px",
+};
+
+const linkStyle = {
+  color: "black",
+  textDecoration: "none",
+  marginRight: "10px",
+};
+
+function Navbar() {
+  return (
+    <nav style={navbarStyle}>
+      <ul>
+        <li>
+          <Link to="/" style={linkStyle}>
+            Aplikacije u ponudi
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
